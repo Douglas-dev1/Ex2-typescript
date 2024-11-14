990d6cccfd597892d1f3d94dc0df9906148e2077
Douglas <douglas.2903@aluno.pr.senac.br>
2024-11-13 21:33:07 -0300

    :balloon: Testes variaveis

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,54 @@
-console.log("ola");
+/**TIPOS DE DADOS */
+/**NUMBER */
+let valor: number = 1;
+
+/**BOOLEANO */
+let valorBol: boolean = false;
+
+let valorTexto = "texto";
+
+let valorIndef = undefined;
+
+let valorNulo = null;
+
+const outroValor = 0;
+
+console.log("Mensagem no terminal");
+
+console.log("Olá mundo");
+
+if(1){
+}else{
+  console.log("");
+}
+
+while (0) {}
+
+do {} while (0);
+{
+
+}
+
+for (let index = 0; index < array.length; index++) {
+  console.log(index); 
+}
+
+const chave = 10;
+
+switch (chave) {
+  case 10:
+    console.log("chave 10");
+    break;
+  default:
+    console.log("Chave invalida");
+    break;
+}
+
+function teste(valor:string) {
+  return "";
+}
+
+let novoValor = 5+3;
+
+novoValor = 5;
+console.log();
